1a5ac123a0e0863e318049aee9bd16c0e3c5e8bf
Xander Adams <xanderadams555@gmail.com>
2025-04-12 09:42:14 -0500

    speed modifications

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -56,6 +56,8 @@ public class CharacterController : MonoBehaviour
 
     private void Jump()
     {
+        movement = rb2D.velocity;
         movement.y = jumpHeight;
+        rb2D.velocity = movement;
     }
 }
